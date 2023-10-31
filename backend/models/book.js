d20eefe9cb4c29fa6a24559d13a8aa765d8e9ef3
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-31 09:51:25 -0300

    ac feita pronto para revisar

--- backend/models/book.js
+++ backend/models/book.js
@@ -4,7 +4,9 @@ const bookSchema = new moongoose.Schema({
   nome: String,
   genero: String,
   sinopse: String,
-  rating: String
+  rating: String,
+  file : File,
+  dataUpload : Date 
 });
 
 const User = moongoose.model("Livros", bookSchema);

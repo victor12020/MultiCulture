7507db04b7d9dcefda60cb09f7556845c9475b70
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-31 10:00:36 -0300

    login refeito

--- backend/models/user.js
+++ backend/models/user.js
@@ -4,6 +4,14 @@ const userSchema = new moongoose.Schema({
   name: String,
   email: String,
   password: String,
+  Telefone : String,
+  endereco: String,
+  complemento : String,
+  numeroDeContato : String,
+  bairro : String,
+  cidade : String,
+  uf : String,
+  cep : String
 });
 
 const User = moongoose.model("Usuario", userSchema);
